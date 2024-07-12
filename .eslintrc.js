38082934a60506174744f652a845f3574d1b72e1
Sunny-jinn <rlawlsdn316@gmail.com>
2024-07-12 16:31:35 +0900

    Initial setting

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
+};
