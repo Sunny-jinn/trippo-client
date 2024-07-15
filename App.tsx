91d068ab5ec67ca7b2d8008e8388209f70b70729
Sunny-jinn <rlawlsdn316@gmail.com>
2024-07-15 15:27:35 +0900

    feat: splash screen 적용

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, Text, useColorScheme} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -9,6 +10,15 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  //!FIX: splach screen 현재 시간초로 되어있는데, loading 완료 후 screen 치우기
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
