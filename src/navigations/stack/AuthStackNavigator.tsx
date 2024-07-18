4045e0fae80f7d7f2db378bb17ff1da307d3f0b2
Sunny-jinn <rlawlsdn316@gmail.com>
2024-07-18 15:14:04 +0900

    feat: android 상태바 배경 색상 변경
    default -> white로 변경 후 폰트 색상 변경 완료

--- src/navigations/stack/AuthStackNavigator.tsx
+++ src/navigations/stack/AuthStackNavigator.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import {authNavigations} from '@/constants';
+import {authNavigations, colors} from '@/constants';
 import {createStackNavigator} from '@react-navigation/stack';
 import LanguageScreen from '@/screens/auth/LanguageScreen';
 import LoginScreen from '@/screens/auth/LoginScreen';
 import SignUpScreen from '@/screens/auth/SignUpScreen';
 import ForgotPasswordScreen from '@/screens/auth/ForgotPasswordScreen';
 import VerificationCodeScreen from '@/screens/auth/VerificationCodeScreen';
+import {StatusBar} from 'react-native';
 
 export type AuthStackParamList = {
   [authNavigations.LANGUAGE]: undefined;
@@ -20,49 +21,52 @@ const Stack = createStackNavigator<AuthStackParamList>();
 
 const AuthStackNavigator = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        cardStyle: {
-          backgroundColor: 'white',
-        },
-        headerShown: false,
-      }}>
-      <Stack.Screen
-        name={authNavigations.LANGUAGE}
-        component={LanguageScreen}
-        options={{
-          headerTitle: '언어 선택',
-        }}
-      />
-      <Stack.Screen
-        name={authNavigations.LOGIN}
-        component={LoginScreen}
-        options={{
-          headerTitle: '로그인',
-        }}
-      />
-      <Stack.Screen
-        name={authNavigations.SIGN_UP}
-        component={SignUpScreen}
-        options={{
-          headerTitle: '회원가입',
-        }}
-      />
-      <Stack.Screen
-        name={authNavigations.FORGOT_PASSWORD}
-        component={ForgotPasswordScreen}
-        options={{
-          headerTitle: '',
-        }}
-      />
-      <Stack.Screen
-        name={authNavigations.VERIFICATION_CODE}
-        component={VerificationCodeScreen}
-        options={{
-          headerTitle: '',
-        }}
-      />
-    </Stack.Navigator>
+    <>
+      <StatusBar backgroundColor={colors.WHITE} barStyle="dark-content" />
+      <Stack.Navigator
+        screenOptions={{
+          cardStyle: {
+            backgroundColor: 'white',
+          },
+          headerShown: false,
+        }}>
+        <Stack.Screen
+          name={authNavigations.LANGUAGE}
+          component={LanguageScreen}
+          options={{
+            headerTitle: '언어 선택',
+          }}
+        />
+        <Stack.Screen
+          name={authNavigations.LOGIN}
+          component={LoginScreen}
+          options={{
+            headerTitle: '로그인',
+          }}
+        />
+        <Stack.Screen
+          name={authNavigations.SIGN_UP}
+          component={SignUpScreen}
+          options={{
+            headerTitle: '회원가입',
+          }}
+        />
+        <Stack.Screen
+          name={authNavigations.FORGOT_PASSWORD}
+          component={ForgotPasswordScreen}
+          options={{
+            headerTitle: '',
+          }}
+        />
+        <Stack.Screen
+          name={authNavigations.VERIFICATION_CODE}
+          component={VerificationCodeScreen}
+          options={{
+            headerTitle: '',
+          }}
+        />
+      </Stack.Navigator>
+    </>
   );
 };
 
